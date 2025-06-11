f631ab7f1c45f140d5e5fc47b8e011515ecaedb3
Pedro <pedrohenriqueteofilo2707@gmail.com>
2025-06-10 21:04:19 -0300

    adicionando o cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const animesRoutes = require('./src/routes/animes');
 const usersRoutes = require('./src/routes/users');
 const authRoutes = require('./src/routes/auth');
@@ -7,7 +8,14 @@ require('./src/models');
 const app = express();
 const port = 4888;
 
+app.use(cors())
 app.use(express.json());
+
+// app.use(cors({
+//     origin: '*', // Permite requisições de qualquer origem
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permite os métodos HTTP especificados
+//     allowedHeaders: ['Content-Type', 'Authorization'] // Permite os cabeçalhos especificados
+// }))
 
 app.use(animesRoutes);
 app.use(usersRoutes);
